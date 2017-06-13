14b94d53a1d6a7db6c0b4efc6635011bded82463
adeb-mirp <adeb-mirp@mit.edu>
2017-06-13 05:35:14 +0530

    star

---
+++ subhradeep/sketch_jun13a.ino
@@ -0,0 +1,15 @@
+int RXled=17;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(RXled, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(RXled, HIGH);   // turn the LED on (HIGH is the voltage level)
+  TXLED1;
+  delay(1000);                       // wait for a second
+  digitalWrite(RXled, LOW);    // turn the LED off by making the voltage LOW
+  TXLED0;
+  delay(1000);                       // wait for a second
+}
